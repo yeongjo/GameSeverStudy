a59a41e437c279746859d1d3c62c0889b9a2e118
sc2466 <songchung2466@gmail.com>
2021-03-31 17:20:52 +0900

    05 iocp서버수정 및 클라이언트 추가

--- 05_StIocpServer/05_StIocpServer.cpp
+++ 05_StIocpServer/05_StIocpServer.cpp
@@ -1,15 +1,15 @@
 ﻿#include <iostream>
 #include <unordered_map>
-using namespace std;
 #include <WS2tcpip.h>
 #include <MSWSock.h> // AcceptEX 사용시 필요
+#include "protocol.h"
+
 #pragma comment(lib, "Ws2_32.lib")
 #pragma comment(lib, "MSWSock.lib")
 
-#define MAX_BUFFER        1024
-#define SERVER_PORT        3500
+using namespace std;
+
 constexpr ULONG_PTR SERVER_ID = 0;
-constexpr int MAX_USER = 10;
 
 enum OP_TYPE {
 	OP_RECV, OP_SEND, OP_ACCEPT
@@ -33,39 +33,26 @@ struct SESSION {
 
 unordered_map <int, SESSION> players;
 
-void CALLBACK recv_callback(DWORD Error, DWORD dataBytes, LPWSAOVERLAPPED overlapped, DWORD lnFlags) {
-	SOCKET client_s = reinterpret_cast<SESSION*>(overlapped)->socket; // 구조체 시작지점이 LPWSAOVERLAPPED이라 가능한 일. 꼼수
-
-	if (dataBytes == 0) {
-		closesocket(players[client_s].socket);
-		players.erase(client_s);
-		return;
-	}  // 클라이언트가 closesocket을 했을 경우
-	cout << "From client : " << players[client_s].messageBuffer << " (" << dataBytes << ") bytes)\n";
-	players[client_s].dataBuffer.len = dataBytes;
-	memset(&(players[client_s].overlapped), 0, sizeof(WSAOVERLAPPED)); // 재사용하기위해 0으로 초기화
-	//players[client_s].overlapped.hEvent = (HANDLE)client_s; // 안쓰기로함
-	WSASend(client_s, &(players[client_s].dataBuffer), 1, NULL, 0, overlapped, send_callback);
+void display_error(const char* msg, int err_no) {
+	WCHAR* lpMsgBuf;
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, err_no, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&lpMsgBuf, 0, NULL);
+	std::cout << msg;
+	std::wcout << lpMsgBuf << std::endl;
+	LocalFree(lpMsgBuf);
 }
 
-void CALLBACK send_callback(DWORD Error, DWORD dataBytes, LPWSAOVERLAPPED overlapped, DWORD lnFlags) {
-	DWORD flags = 0;
-
-	SOCKET client_s = reinterpret_cast<SESSION*>(overlapped)->socket;
-
-	if (dataBytes == 0) {
-		closesocket(players[client_s].socket);
-		players.erase(client_s);
-		return;
-	}  // 클라이언트가 closesocket을 했을 경우
-
-	// WSASend(응답에 대한)의 콜백일 경우
-
-	cout << "TRACE - Send message : " << players[client_s].messageBuffer << " (" << dataBytes << " bytes)\n";
-	memset(&(players[client_s].overlapped), 0, sizeof(WSAOVERLAPPED));
-	players[client_s].dataBuffer.len = MAX_BUFFER;
-	//players[client_s].overlapped.hEvent = (HANDLE)client_s; // 안쓰기로함
-	WSARecv(client_s, &players[client_s].dataBuffer, 1, 0, &flags, overlapped, recv_callback);
+void send_packet(int p_id, void *p) {
+	int p_size = static_cast<unsigned char*>(p)[0];
+	int p_type = static_cast<unsigned char*>(p)[1];
+	cout << "To client [" << p_id << "] : ";
+	cout << "Pakcet [" << p_type << "]\n";
+	EX_OVER* s_over = new EX_OVER;
+	s_over->m_op = OP_SEND;
+	memset(&s_over->m_over, 0, sizeof(s_over->m_over));
+	memcpy(s_over->m_packetbuf, p, p_size);
+	s_over->m_wasBuffer[0].buf = reinterpret_cast<char*>(&s_over->m_packetbuf);
+	s_over->m_wasBuffer[0].len = p_size;
+	WSASend(players[p_id].m_socket, s_over->m_wasBuffer, 1, NULL, 0, &s_over->m_over, 0);
 }
 
 void do_recv(int key) {
@@ -80,25 +67,88 @@ void do_recv(int key) {
 int get_new_player_id() {
 	for (int i = SERVER_ID + 1; i < MAX_USER; ++i) // 0은 서버의 id여서 쓰면 안된다.
 	{
-		if (i == players.size()) 
+		if (i == players.size() + 1) 
 			return i;
 	}
+	return -1;
 }
 
 
-void processPacket(ULONG_PTR p_id, unsigned char* p_buf)
+void send_login_ok_packet(int p_id)
+{
+	s2c_login_ok p;
+	p.hp = 10;
+	p.id = p_id;
+	p.level = 2;
+	p.race = 1;
+	p.size = sizeof(p);
+	p.type = S2C_LOGIN_OK;
+	p.x = players[p_id].x;
+	p.y = players[p_id].y;
+	send_packet(p_id, &p);
+}
+
+void send_move_packet(int p_id)
+{
+	s2c_move_player p;
+	p.id = p_id;
+	p.size = sizeof(p);
+	p.type = S2C_MOVE_PLAYER;
+	p.x = players[p_id].x;
+	p.y = players[p_id].y;
+	send_packet(p_id, &p);
+}
+
+void do_move(int p_id, DIRECTION direction)
+{
+	auto& x = players[p_id].x;
+	auto& y = players[p_id].y;
+	switch(direction)
+	{
+	case D_N:
+		y > 0 ? y-- : y;
+		break;
+	case D_S:
+		y < WORLD_Y_SIZE ? y++ : y;
+		break;
+	case D_E:
+		x < WORLD_X_SIZE ? x++ : x;
+		break;
+	case D_W:
+		x > 0 ? x-- : x;
+		break;
+	}
+}
+
+void processPacket(int p_id, unsigned char* p_buf)
 {
 	switch(p_buf[1])
 	{
 	case C2S_LOGIN:
-		break;
+		{
+			c2s_login* packet = reinterpret_cast<c2s_login*>(p_buf);
+			strcpy_s(players[p_id].m_name, packet->name);
+			send_login_ok_packet(p_id);
+			break;
+		}
 	case C2S_MOVE:
-		break;
+		{
+			c2s_move* packet = (c2s_move*)p_buf;
+			do_move(p_id, packet->direction);
+			break;
+		}
 	default:
-		cout << "Unknown Packet type from client[" << p_id << "] Packet type[" << (int)p_buf[1] << endl;
-		while (true);
-		break;
+		{
+			cout << "Unknown Packet type from client[" << p_id << "] Packet type[" << (int)p_buf[1] << endl;
+			while (true);
+			break;
+		}
 	}
+}
+
+void disconnect(int p_id) {
+	closesocket(players[p_id].m_socket);
+	players.erase(p_id);
 }
 
 int main() {
@@ -120,13 +170,26 @@ int main() {
 	memset(&accept_over.m_over, 0, sizeof(accept_over.m_over));
 	SOCKET c_socket = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
 	//AcceptEX
-	AcceptEx(listenSocket, c_socket, accept_over.m_packetbuf, 0, 16, 16, NULL, &accept_over.m_over);
+	AcceptEx(listenSocket, c_socket, accept_over.m_packetbuf, 0, 32, 32, NULL, &accept_over.m_over);
 
 	while (true) {
 		DWORD num_bytes;
-		ULONG_PTR key;
+		ULONG_PTR ikey;
 		WSAOVERLAPPED* over;
-		BOOL ret = GetQueuedCompletionStatus(h_iocp, &num_bytes, &key, &over, INFINITE);
+		BOOL ret = GetQueuedCompletionStatus(h_iocp, &num_bytes, &ikey, &over, INFINITE);
+		int key = static_cast<int>(ikey);
+
+		if (FALSE == ret) {
+			if(SERVER_ID == key)
+			{
+				display_error("Server GQCS: ", WSAGetLastError());
+				exit(-1);
+			}else
+			{
+				display_error("GQCS: ", WSAGetLastError());
+				disconnect(key);
+			}
+		}
 
 		EX_OVER* ex_over = reinterpret_cast<EX_OVER*>(over);
 
@@ -143,6 +206,8 @@ int main() {
 					processPacket(key, packet_ptr);
 					num_data -= packet_size;
 					packet_ptr += packet_size;
+					if (0 >= num_data)
+						break;
 					packet_size = packet_ptr[0];
 				}
 				players[key].m_prev_size = num_data;
@@ -161,25 +226,30 @@ int main() {
 		case OP_ACCEPT:
 			{
 				int  c_id = get_new_player_id();
-				players[c_id] = SESSION{};
-				players[c_id].id = c_id;
-				players[c_id].m_name[0] = 0;
-				players[c_id].m_recv_over.m_op = OP_RECV;
-				players[c_id].m_recv_over.m_wasBuffer[0].len = MAX_BUFFER;
-				players[c_id].m_recv_over.m_wasBuffer[0].buf = reinterpret_cast<char*>(&players[c_id].m_recv_over.m_packetbuf);
-				players[c_id].m_socket = c_socket;
-				players[c_id].m_prev_size = 0;
-				do_recv(c_id);
+				if(c_id != -1)
+				{
+					players[c_id] = SESSION{};
+					players[c_id].id = c_id;
+					players[c_id].m_name[0] = 0;
+					players[c_id].m_recv_over.m_op = OP_RECV;
+					players[c_id].m_recv_over.m_wasBuffer[0].len = MAX_BUFFER;
+					players[c_id].m_recv_over.m_wasBuffer[0].buf = reinterpret_cast<char*>(&players[c_id].m_recv_over.m_packetbuf);
+					players[c_id].m_socket = c_socket;
+					players[c_id].m_prev_size = 0;
+					CreateIoCompletionPort(reinterpret_cast<HANDLE>(c_socket), h_iocp, c_id, 0);
+					do_recv(c_id);
 
-				memset(&accept_over.m_over, 0, sizeof(accept_over.m_over));
-				SOCKET c_socket = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
-				//AcceptEX
-				AcceptEx(listenSocket, c_socket, accept_over.m_packetbuf, 0, 16, 16, NULL, &accept_over.m_over);
+					memset(&accept_over.m_over, 0, sizeof(accept_over.m_over));
+					SOCKET c_socket = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+					//AcceptEX
+					AcceptEx(listenSocket, c_socket, accept_over.m_packetbuf, 0, 32, 32, NULL, &accept_over.m_over);
+				}else
+				{
+					closesocket(c_socket);
+				}
 				break;
 			}
 		}
-
-
 	}
 	closesocket(listenSocket);
 	WSACleanup();
--- 05_StIocpServer/protocol.h
+++ 05_StIocpServer/protocol.h
@@ -6,6 +6,11 @@ constexpr unsigned char S2C_ADD_PLAYER = 4;
 constexpr unsigned char S2C_MOVE_PLAYER = 5;
 constexpr unsigned char S2C_REMOVE_PLAYER = 6;
 constexpr int MAX_NAME = 100;
+constexpr int MAX_BUFFER = 1024;
+constexpr short SERVER_PORT = 3500;
+constexpr int WORLD_X_SIZE = 8;
+constexpr int WORLD_Y_SIZE = 8;
+constexpr auto MAX_USER = 10;
 
 #pragma pack(push, 1)
 
@@ -18,7 +23,7 @@ public:
 };
 
 enum DIRECTION {
-	D_N, D_S, D_W, D_E,
+	D_NO, D_N, D_S, D_W, D_E,
 };
 
 struct c2s_move
@@ -27,6 +32,38 @@ public:
 	unsigned char size;
 	unsigned char type;
 	DIRECTION direction; // 0: North, 1: South, 2:
+};
+
+struct s2c_login_ok
+{
+	unsigned char size;
+	unsigned char type;
+	int	id;
+	short	x, y;
+	int hp, level;
+	int race;
+};
+
+struct s2c_add_player {
+	unsigned char size;
+	unsigned char type;
+	int	id;
+	short	x, y;
+	int race;
+};
+
+struct s2c_move_player
+{
+	unsigned char size;
+	unsigned char type;
+	int	id;
+	short	x, y;
+};
+
+struct s2c_remove_player {
+	unsigned char size;
+	unsigned char type;
+	int	id;
 };
 
 #pragma pack(pop)
