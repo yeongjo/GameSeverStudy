013e4bf1cd9e37306fb2f87bd684738d1bf7c8df
sc2466 <songchung2466@gmail.com>
2021-04-23 14:12:04 +0900

    데드락 예시

--- 08_MultiThreadTest2/08_MultiThreadTest2.cpp
+++ 08_MultiThreadTest2/08_MultiThreadTest2.cpp
@@ -1,24 +1,31 @@
 ﻿#include <iostream>
+#include <mutex>
 #include <thread>
 
 using namespace std;
 
 volatile bool flag = false;
 int sync_data = 0;
+mutex ss;
 
 void consumer() {
+	ss.lock();
 	while (flag == false);
+	ss.unlock();
 	cout << "i received" << sync_data << endl;
 }
 
 void producer() {
 	sync_data = 999;
+	ss.lock();
 	flag = true;
+	ss.unlock();
 }
 
 
 void main() {
 	thread con{ consumer };
+	this_thread::sleep_for(1s);
 	thread pro{ producer };
 	con.join();
 	pro.join();
