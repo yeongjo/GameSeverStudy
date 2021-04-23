9d4f2633d9df6b5277d14aa721bb816f79a0e55a
sc2466 <songchung2466@gmail.com>
2021-04-23 14:08:17 +0900

    volatile로 최적화 방지해서 정상적으로 프로그램 실행되게 만듬 https://popcorntree.tistory.com/90?category=813524

---
+++ 08_MultiThreadTest2/08_MultiThreadTest2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <thread>
+
+using namespace std;
+
+volatile bool flag = false;
+int sync_data = 0;
+
+void consumer() {
+	while (flag == false);
+	cout << "i received" << sync_data << endl;
+}
+
+void producer() {
+	sync_data = 999;
+	flag = true;
+}
+
+
+void main() {
+	thread con{ consumer };
+	thread pro{ producer };
+	con.join();
+	pro.join();
+}
