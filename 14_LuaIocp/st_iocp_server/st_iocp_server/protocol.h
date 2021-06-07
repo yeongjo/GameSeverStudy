d70bee2a23bc95153e2086c0e0ee26240db944ca
song chung <songchung2466@gmail.com>
2021-06-08 05:41:52 +0900

    프로토콜 인원수 수정

--- 14_LuaIocp/st_iocp_server/st_iocp_server/protocol.h
+++ 14_LuaIocp/st_iocp_server/st_iocp_server/protocol.h
@@ -3,14 +3,14 @@
 constexpr int MAX_NAME = 100;
 constexpr int MAX_BUFFER = 256;
 constexpr short SERVER_PORT = 3500;
-//constexpr int WORLD_X_SIZE = 4000;
-//constexpr int WORLD_Y_SIZE = 4000;
-//constexpr int NPC_START = 10000;
-//constexpr int MAX_USER = 200000 + NPC_START;
-constexpr int WORLD_X_SIZE = 1000;
-constexpr int WORLD_Y_SIZE = 1000;
-constexpr int NPC_START = 1000;
-constexpr int MAX_USER = 5000 + NPC_START;
+constexpr int WORLD_X_SIZE = 4000;
+constexpr int WORLD_Y_SIZE = 4000;
+constexpr int NPC_START = 10000;
+constexpr int MAX_USER = 200000 + NPC_START;
+//constexpr int WORLD_X_SIZE = 1000;
+//constexpr int WORLD_Y_SIZE = 1000;
+//constexpr int NPC_START = 1000;
+//constexpr int MAX_USER = 5000 + NPC_START;
 
 constexpr int VIEW_RADIUS = 5;
 
